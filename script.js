51c2bd4a1ee3fa30e889c4f1dca79d34d884f783
Hack Reactor Student <student@hackreactor.com>
2017-08-09 12:31:04 -0500

    made effects after a button is pressed

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
-
+$("#pink").click(function(){
+  $(".container").css('border', "solid 20px pink");
+});
+$("#blue").click(function(){
+  $(".container").css('background-color', "blue");
+});
+$("#fade").click(function(){
+  $(".container").fadeOut("slow");
+});
+$("#lottery").click(function(){
+  alert("YOU WON THE LOTTERY!");
+});
+$("#confirm").click(function(){
+  confirm("Are you sure?");
+});
+$("#puppy").click(function(){
+  $("body").css('background-image', "url('https://cdn.pixabay.com/photo/2015/02/05/12/09/chihuahua-624924_960_720.jpg')");
+});
+$("#replace").click(function(){
+  $(this).replaceWith("<div><center><h2>REPLACED!</h2></center></div>");
+});
+$("#drive").click(function(){
+  var age = prompt("How old are you?");
+  if(age >= 16){
+    $(".container").replaceWith("<center><h1>You're old enough to drive!</h1></center>");
+  }else{
+    $(".container").replaceWith("<center><h1>You're not old enough to drive.</h1></center>");
+  }
+  });
+  $("#choose").click(function(){
+    $(this).replaceWith("<div><center><h2>REPLACED!</h2></center></div>");
+  });
 })
